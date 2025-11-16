4bf453941278cc060a1ec91b28205e2bcb56e5bb
Ken Hazlewood <khazlewo@outlook.com>
2025-11-16 10:31:15 -0800

    Initial PWA project upload

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to MovieFinder!</h2>
+      <p>Use the search page to find movie information using the TMDB API.</p>
+    </div>
+  );
+}
+
+export default Home;
